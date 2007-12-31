c875dae79aa5220feece86efdda1ba09328995a7
William Hubbs <w.d.hubbs@gmail.com>
2007-12-31 19:30:22 +0000

    Initial setup to work with distutils

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='pybugz',
-    version='0.8.0',
-    py_modules=['bugz'],
-    scripts = ['bugz']
+    name = 'pybugz',
+    author = 'Alastair Tse',
+    author_email = 'alastair@liquidx.net',
+    url = 'http://pybugz.googlecode.com',
+    version = '0.8.0',
+    packages = ['bugz'],
+    package_data = {'bugz': ['bugzdefs/*']},
+    scripts = ['scripts/bugz']
 )
