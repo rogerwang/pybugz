4e63350c84bb35bfd13117e87bb9ad5971b61866
William Hubbs <w.d.hubbs@gmail.com>
2009-03-17 20:24:51 -0500

    reverted version number
    I set the version number in the master branch to match 0.7.x hopefully
    to make it easier to merge the 0.7 branch when we decide to do so.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'pybugz',
-    version = '0.8.0',
+    version = '0.7.4',
     author = 'Alastair Tse',
     author_email = 'alastair@liquidx.net',
     url = 'http://pybugz.googlecode.com',
